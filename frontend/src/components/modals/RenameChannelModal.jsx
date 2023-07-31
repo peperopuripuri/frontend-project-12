7a47c182cf48c705657f7b976620a178782cda48
PuriPuri <aliceicelie@gmail.com>
2023-07-31 10:53:06 +0300

    fix linter

--- frontend/src/components/modals/RenameChannelModal.jsx
+++ frontend/src/components/modals/RenameChannelModal.jsx
@@ -1,8 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  Form, Button, Modal, FormText,
-} from 'react-bootstrap';
+import { Form, Button, Modal, FormText } from 'react-bootstrap';
 import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
@@ -10,13 +8,15 @@ import * as Yup from 'yup';
 import { hideModal } from '../../store/slices/modalsSlice';
 import useSocketApi from '../../hooks/useSocketApi.hook';
 
-const renderFormFeedback = (formik) => formik.errors.name && formik.touched.name && (
-  <FormText className="feedback text-danger mt-3">
-    {formik.errors.name}
-  </FormText>
-);
+const renderFormFeedback = (formik) =>
+  formik.errors.name &&
+  formik.touched.name && (
+    <FormText className="feedback text-danger mt-3">
+      {formik.errors.name}
+    </FormText>
+  );
 
-const getValidation = (t, channels) => (
+const getValidation = (t, channels) =>
   Yup.object({
     name: Yup.string()
       .min(3, t('renameChannelModal.validation.min'))
@@ -25,69 +25,65 @@ const getValidation = (t, channels) => (
         t('renameChannelModal.validation.unique'),
       )
       .required(t('renameChannelModal.validation.required')),
-  })
-);
+  });
 
-const GetFormik = (validationSchema, handleRename, currentChannelName) => (
+const GetFormik = (validationSchema, handleRename, currentChannelName) =>
   useFormik({
     initialValues: {
       name: currentChannelName,
     },
     validationSchema,
     onSubmit: handleRename,
-  })
-);
+  });
 
-const RenderModal = ({
-  t, handleSubmit, formik, inputEl, dispatch,
-}) => (
+const RenderModal = ({ t, handleSubmit, formik, inputEl, dispatch }) => (
   <Modal show onHide={() => dispatch(hideModal())}>
-      <Modal.Header closeButton>
-        <Modal.Title>{t('renameChannelModal.header')}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Control
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              ref={inputEl}
-              aria-label="form"
-              name="name"
-              type="text"
-              disabled={formik.isSubmitting}
-              autoFocus
-              autoComplete="off"
-              isInvalid={formik.errors.name && formik.touched.name}
-            />
-            <Form.Label className="visually-hidden" htmlFor="name">
-              {t('renameChannelModal.label')}
-            </Form.Label>
-            {renderFormFeedback(formik)}
-          </Form.Group>
-          <div>
-            <Button
-              className="m-1"
-              role="button"
-              disabled={formik.isSubmitting}
-              variant="secondary"
-              onClick={() => dispatch(hideModal())}
-            >
-              {t('renameChannelModal.cancel')}
-            </Button>
-            <Button
-              className="m-1"
-              variant="primary"
-              disabled={formik.isSubmitting}
-              role="button"
-              type="submit"
-            >
-              {t('renameChannelModal.rename')}
-            </Button>
-          </div>
-        </Form>
-      </Modal.Body>
-    </Modal>
+    <Modal.Header closeButton>
+      <Modal.Title>{t('renameChannelModal.header')}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Control
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            ref={inputEl}
+            aria-label="form"
+            name="name"
+            type="text"
+            disabled={formik.isSubmitting}
+            autoFocus
+            autoComplete="off"
+            isInvalid={formik.errors.name && formik.touched.name}
+          />
+          <Form.Label className="visually-hidden" htmlFor="name">
+            {t('renameChannelModal.label')}
+          </Form.Label>
+          {renderFormFeedback(formik)}
+        </Form.Group>
+        <div>
+          <Button
+            className="m-1"
+            role="button"
+            disabled={formik.isSubmitting}
+            variant="secondary"
+            onClick={() => dispatch(hideModal())}
+          >
+            {t('renameChannelModal.cancel')}
+          </Button>
+          <Button
+            className="m-1"
+            variant="primary"
+            disabled={formik.isSubmitting}
+            role="button"
+            type="submit"
+          >
+            {t('renameChannelModal.rename')}
+          </Button>
+        </div>
+      </Form>
+    </Modal.Body>
+  </Modal>
 );
 
 const RenameChannelModal = () => {
