500e216490b7dd6fbb34dcb80d1325ef5e44db5c
PuriPuri <aliceicelie@gmail.com>
2023-07-31 22:11:47 +0300

    fix linter

--- frontend/src/components/modals/AddChannelModal.jsx
+++ frontend/src/components/modals/AddChannelModal.jsx
@@ -11,7 +11,44 @@ import useSocketApi from '../../hooks/useSocketApi.hook';
 import * as channelActions from '../../store/slices/channelsSlice';
 import { hideModal } from '../../store/slices/modalsSlice';
 
-const AddChannelModal = () => {
+function RenderModal({ t, formik, dispatch }) {
+  return (
+    <Modal show>
+      <Modal.Header closeButton onHide={() => dispatch(hideModal())}>
+        <Modal.Title>{t('addChannelModal.header')}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Control
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              type="text"
+              name="name"
+              disabled={formik.isSubmitting}
+              placeholder={t('addChannelModal.label')}
+              autoFocus
+              isInvalid={formik.errors.name && formik.touched.name}
+            />
+            <Form.Label className="visually-hidden" htmlFor="name">{t('addChannelModal.label')}</Form.Label>
+
+            {formik.errors.name && formik.touched.name && (
+            <FormText className="feedback text-danger mt-3">{formik.errors.name}</FormText>
+            )}
+          </Form.Group>
+          <Button variant="secondary" onClick={() => dispatch(hideModal())} className="m-1">
+            {t('addChannelModal.cancel')}
+          </Button>
+          <Button variant="primary" onClick={formik.handleSubmit} type="submit" className="m-1">
+            {t('addChannelModal.submit')}
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+function AddChannelModal() {
   const channels = useSelector((state) => state.channels.channels);
   const dispatch = useDispatch();
   const chatApi = useSocketApi();
@@ -49,40 +86,7 @@ const AddChannelModal = () => {
     validationSchema: validate,
   });
 
-  return (
-    <Modal show>
-      <Modal.Header closeButton onHide={() => dispatch(hideModal())}>
-        <Modal.Title>{t('addChannelModal.header')}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={formik.handleSubmit}>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Control
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              type="text"
-              name="name"
-              disabled={formik.isSubmitting}
-              placeholder={t('addChannelModal.label')}
-              autoFocus
-              isInvalid={formik.errors.name && formik.touched.name}
-            />
-            <Form.Label className="visually-hidden" htmlFor="name">{t('addChannelModal.label')}</Form.Label>
-
-            {formik.errors.name && formik.touched.name && (
-              <FormText className="feedback text-danger mt-3">{formik.errors.name}</FormText>
-            )}
-          </Form.Group>
-          <Button variant="secondary" onClick={() => dispatch(hideModal())} className="m-1">
-            {t('addChannelModal.cancel')}
-          </Button>
-          <Button variant="primary" onClick={formik.handleSubmit} type="submit" className="m-1">
-            {t('addChannelModal.submit')}
-          </Button>
-        </Form>
-      </Modal.Body>
-    </Modal>
-  );
-};
+  return <RenderModal t={t} formik={formik} dispatch={dispatch} />;
+}
 
 export default AddChannelModal;
