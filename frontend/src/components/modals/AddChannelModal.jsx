c8b16fb3fd3477e659b4e007e2d8740fe18a975a
PuriPuri <aliceicelie@gmail.com>
2023-07-31 22:15:57 +0300

    fix linter

--- frontend/src/components/modals/AddChannelModal.jsx
+++ frontend/src/components/modals/AddChannelModal.jsx
@@ -11,44 +11,55 @@ import useSocketApi from '../../hooks/useSocketApi.hook';
 import * as channelActions from '../../store/slices/channelsSlice';
 import { hideModal } from '../../store/slices/modalsSlice';
 
-function RenderModal({ t, formik, dispatch }) {
-  return (
-    <Modal show>
-      <Modal.Header closeButton onHide={() => dispatch(hideModal())}>
-        <Modal.Title>{t('addChannelModal.header')}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={formik.handleSubmit}>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Control
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              type="text"
-              name="name"
-              disabled={formik.isSubmitting}
-              placeholder={t('addChannelModal.label')}
-              autoFocus
-              isInvalid={formik.errors.name && formik.touched.name}
-            />
-            <Form.Label className="visually-hidden" htmlFor="name">{t('addChannelModal.label')}</Form.Label>
+const RenderModal = ({ t, formik, dispatch }) => (
+  <Modal show>
+    <Modal.Header closeButton onHide={() => dispatch(hideModal())}>
+      <Modal.Title>{t('addChannelModal.header')}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Control
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            type="text"
+            name="name"
+            disabled={formik.isSubmitting}
+            placeholder={t('addChannelModal.label')}
+            autoFocus
+            isInvalid={formik.errors.name && formik.touched.name}
+          />
+          <Form.Label className="visually-hidden" htmlFor="name">
+            {t('addChannelModal.label')}
+          </Form.Label>
 
-            {formik.errors.name && formik.touched.name && (
-            <FormText className="feedback text-danger mt-3">{formik.errors.name}</FormText>
-            )}
-          </Form.Group>
-          <Button variant="secondary" onClick={() => dispatch(hideModal())} className="m-1">
-            {t('addChannelModal.cancel')}
-          </Button>
-          <Button variant="primary" onClick={formik.handleSubmit} type="submit" className="m-1">
-            {t('addChannelModal.submit')}
-          </Button>
-        </Form>
-      </Modal.Body>
-    </Modal>
-  );
-}
+          {formik.errors.name && formik.touched.name && (
+            <FormText className="feedback text-danger mt-3">
+              {formik.errors.name}
+            </FormText>
+          )}
+        </Form.Group>
+        <Button
+          variant="secondary"
+          onClick={() => dispatch(hideModal())}
+          className="m-1"
+        >
+          {t('addChannelModal.cancel')}
+        </Button>
+        <Button
+          variant="primary"
+          onClick={formik.handleSubmit}
+          type="submit"
+          className="m-1"
+        >
+          {t('addChannelModal.submit')}
+        </Button>
+      </Form>
+    </Modal.Body>
+  </Modal>
+);
 
-function AddChannelModal() {
+const AddChannelModal = () => {
   const channels = useSelector((state) => state.channels.channels);
   const dispatch = useDispatch();
   const chatApi = useSocketApi();
@@ -57,26 +68,32 @@ function AddChannelModal() {
   const validate = Yup.object({
     name: Yup.string()
       .min(3, t('addChannelModal.validation.min'))
-      .notOneOf(channels.map((c) => c.name), t('addChannelModal.validation.unique'))
+      .notOneOf(
+        channels.map((c) => c.name),
+        t('addChannelModal.validation.unique'),
+      )
       .required(t('addChannelModal.validation.required')),
   });
 
-  const handleSubmit = useCallback(async (values, { resetForm }) => {
-    const channelData = {
-      name: values.name,
-      removable: true,
-    };
+  const handleSubmit = useCallback(
+    async (values, { resetForm }) => {
+      const channelData = {
+        name: values.name,
+        removable: true,
+      };
 
-    try {
-      const response = await chatApi.createChannel(channelData);
-      toast.success(t('toast.add'));
-      dispatch(channelActions.setCurrentChannelId(response.id));
-      resetForm({ values: '' });
-      dispatch(hideModal());
-    } catch (error) {
-      toast.error(t('toast.error'));
-    }
-  }, [chatApi, dispatch, t]);
+      try {
+        const response = await chatApi.createChannel(channelData);
+        toast.success(t('toast.add'));
+        dispatch(channelActions.setCurrentChannelId(response.id));
+        resetForm({ values: '' });
+        dispatch(hideModal());
+      } catch (error) {
+        toast.error(t('toast.error'));
+      }
+    },
+    [chatApi, dispatch, t],
+  );
 
   const formik = useFormik({
     initialValues: {
@@ -87,6 +104,6 @@ function AddChannelModal() {
   });
 
   return <RenderModal t={t} formik={formik} dispatch={dispatch} />;
-}
+};
 
 export default AddChannelModal;
