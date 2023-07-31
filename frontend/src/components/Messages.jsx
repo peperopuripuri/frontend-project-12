996f51d1796b7b15ae6f19fe6286f944a8ddc522
PuriPuri <aliceicelie@gmail.com>
2023-07-31 21:39:53 +0300

    fix linter

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -15,8 +15,8 @@ const MessageItem = ({ msg, user }) => {
   return (
     <div key={id} className="text-break mb-2">
       <b className={isCurrentUser ? 'text-white bg-dark' : ''}>{username}</b>
-      :
-      {' '}
+:
+{' '}
       {body}
     </div>
   );
@@ -25,34 +25,34 @@ const MessageItem = ({ msg, user }) => {
 const RenderForm = ({
   t, formHandler, controlInput, inputValue,
 }) => (
-    <Form onSubmit={formHandler} noValidate className="py-1 border rounded-2">
-      <InputGroup hasValidation>
-        <Form.Control
-          onChange={controlInput}
-          name="body"
-          aria-label={t('messages.label')}
-          placeholder={t('messages.placeholder')}
-          className="border-0 p-0 ps-2"
-          value={inputValue}
-        />
-        <Button type="submit" disabled={!inputValue} variant="outline-secondary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            width="20"
-            height="20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-            />
-          </svg>
-          <span className="visually-hidden">{t('messages.send')}</span>
-        </Button>
-      </InputGroup>
-    </Form>
-  );
+  <Form onSubmit={formHandler} noValidate className="py-1 border rounded-2">
+    <InputGroup hasValidation>
+      <Form.Control
+        onChange={controlInput}
+        name="body"
+        aria-label={t('messages.label')}
+        placeholder={t('messages.placeholder')}
+        className="border-0 p-0 ps-2"
+        value={inputValue}
+      />
+      <Button type="submit" disabled={!inputValue} variant="outline-secondary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          width="20"
+          height="20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+          />
+        </svg>
+        <span className="visually-hidden">{t('messages.send')}</span>
+      </Button>
+    </InputGroup>
+  </Form>
+);
 
 const Messages = () => {
   const chatApi = useSocketApi();
@@ -98,7 +98,7 @@ const Messages = () => {
         <p className="m-0">
           <b>
             {t('messages.id')}
-            {' '}
+{' '}
             {currentChannel ? currentChannel.name : t('messages.loading')}
           </b>
         </p>
