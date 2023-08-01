3617a8c2d2da62d77fd3d8b91b49d7f788168cc9
PuriPuri <aliceicelie@gmail.com>
2023-08-01 14:47:07 +0300

    delete linter and codeclimate Channels

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -6,9 +6,7 @@ import cn from 'classnames';
 import { setCurrentChannelId } from '../store/slices/channelsSlice';
 import { showModal } from '../store/slices/modalsSlice';
 
-const ChannelButton = ({
-  id, name, currentChannelId, t, onClick,
-}) => {
+const ChannelButton = ({ id, name, currentChannelId, t, onClick }) => {
   const isActive = id === currentChannelId;
   const activeClassName = cn('btn', {
     'btn-secondary': isActive,
@@ -29,10 +27,9 @@ const ChannelButton = ({
   );
 };
 
-const RemovableChannel = ({
-  id, name, currentChannelId, t, dispatch,
-}) => {
-  const handlerVariant = () => (id === currentChannelId ? 'secondary' : 'outline-secondary');
+const RemovableChannel = ({ id, name, currentChannelId, t, dispatch }) => {
+  const handlerVariant = () =>
+    id === currentChannelId ? 'secondary' : 'outline-secondary';
 
   const handleChannelClick = () => {
     dispatch(setCurrentChannelId(id));
@@ -81,6 +78,37 @@ const RemovableChannel = ({
   );
 };
 
+const RenderCol = ({ handleAddChannelClick, renderChannels, t }) => (
+  <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+    <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+      <b>{t('channels.title')}</b>
+      <button
+        onClick={handleAddChannelClick}
+        type="button"
+        className="p-0 text-primary btn btn-group-vertical"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          width="20"
+          height="20"
+          fill="currentColor"
+        >
+          <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+        </svg>
+        <span className="visually-hidden">{t('channels.plus')}</span>
+      </button>
+    </div>
+    <ul
+      id="channels-box"
+      className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
+    >
+      {renderChannels()}
+    </ul>
+  </Col>
+);
+
 const Channels = () => {
   const dispatch = useDispatch();
   const { channels, currentChannelId } = useSelector((state) => state.channels);
@@ -94,23 +122,23 @@ const Channels = () => {
     dispatch(showModal({ modalType: 'adding', channelId: null }));
   };
 
-  const renderChannels = () => channels.map(({ id, name, removable }) => {
-    if (!removable) {
-      return (
-        <li key={id} className="nav-item w-100">
-          <ChannelButton
-            id={id}
-            name={name}
-            currentChannelId={currentChannelId}
-            t={t}
-            onClick={handleChannelClick}
-          />
-        </li>
-      );
-    }
+  const renderChannels = () =>
+    channels.map(({ id, name, removable }) => {
+      if (!removable) {
+        return (
+          <li key={id} className="nav-item w-100">
+            <ChannelButton
+              id={id}
+              name={name}
+              currentChannelId={currentChannelId}
+              t={t}
+              onClick={handleChannelClick}
+            />
+          </li>
+        );
+      }
 
-    return (
-        // eslint-disable-next-line react/jsx-indent
+      return (
         <RemovableChannel
           key={id}
           id={id}
@@ -119,38 +147,15 @@ const Channels = () => {
           t={t}
           dispatch={dispatch}
         />
-    );
-  });
+      );
+    });
 
   return (
-    <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-      <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-        <b>{t('channels.title')}</b>
-        <button
-          onClick={handleAddChannelClick}
-          type="button"
-          className="p-0 text-primary btn btn-group-vertical"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            width="20"
-            height="20"
-            fill="currentColor"
-          >
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-          </svg>
-          <span className="visually-hidden">{t('channels.plus')}</span>
-        </button>
-      </div>
-      <ul
-        id="channels-box"
-        className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
-      >
-        {renderChannels()}
-      </ul>
-    </Col>
+    <RenderCol
+      handleAddChannelClick={handleAddChannelClick}
+      renderChannels={renderChannels}
+      t={t}
+    />
   );
 };
 
