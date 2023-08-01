a2d4ef42bfe6f80d050a11b90df3f9cc214bda3d
PuriPuri <aliceicelie@gmail.com>
2023-08-01 19:33:04 +0300

    Removechannelmodal codeclimate

--- frontend/src/components/modals/RemoveChannelModal.jsx
+++ frontend/src/components/modals/RemoveChannelModal.jsx
@@ -6,6 +6,35 @@ import { toast } from 'react-toastify';
 import { hideModal } from '../../store/slices/modalsSlice';
 import useSocketApi from '../../hooks/useSocketApi.hook';
 
+const RenderModal = ({ dispatch, t, handleRemove }) => (
+  <Modal show onHide={() => dispatch(hideModal())}>
+    <Modal.Header closeButton>
+      <Modal.Title>{t('removeChannelModal.header')}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <p className="lead">{t('removeChannelModal.body')}</p>
+      <div>
+        <Button
+          className="m-1"
+          variant="secondary"
+          role="button"
+          onClick={() => dispatch(hideModal())}
+        >
+          {t('removeChannelModal.cancel')}
+        </Button>
+        <Button
+          className="m-1"
+          variant="danger"
+          role="button"
+          onClick={handleRemove}
+        >
+          {t('removeChannelModal.delete')}
+        </Button>
+      </div>
+    </Modal.Body>
+  </Modal>
+);
+
 const RemoveChannelModal = () => {
   const dispatch = useDispatch();
   const channelId = useSelector((state) => state.channels.currentChannelId);
@@ -22,34 +51,7 @@ const RemoveChannelModal = () => {
     }
   }, [chatApi, channelId, dispatch, t]);
 
-  return (
-    <Modal show onHide={() => dispatch(hideModal())}>
-      <Modal.Header closeButton>
-        <Modal.Title>{t('removeChannelModal.header')}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p className="lead">{t('removeChannelModal.body')}</p>
-        <div>
-          <Button
-            className="m-1"
-            variant="secondary"
-            role="button"
-            onClick={() => dispatch(hideModal())}
-          >
-            {t('removeChannelModal.cancel')}
-          </Button>
-          <Button
-            className="m-1"
-            variant="danger"
-            role="button"
-            onClick={handleRemove}
-          >
-            {t('removeChannelModal.delete')}
-          </Button>
-        </div>
-      </Modal.Body>
-    </Modal>
-  );
+  return <RenderModal dispatch={dispatch} t={t} handleRemove={handleRemove} />;
 };
 
 export default RemoveChannelModal;
