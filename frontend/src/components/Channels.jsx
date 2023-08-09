b7973851ae0c8a9fbe15302c0981c4e362ce5222
PuriPuri <aliceicelie@gmail.com>
2023-08-09 12:29:02 +0300

    fix logic

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,82 +1,71 @@
 import React from 'react';
 import {
-  Col, Dropdown, Button,
+  Col, ButtonGroup, Dropdown, Button,
 } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
 import { setCurrentChannelId } from '../store/slices/channelsSlice';
 import { showModal } from '../store/slices/modalsSlice';
-import { selectChannels, selectCurrentChannelId } from '../store/slices/selectors';
 
-const ChannelButton = ({
-  id, name, currentChannelId, t, onClick,
-}) => {
-  const isActive = id === currentChannelId;
+const renderChannelItem = (channel, currentChannelId, dispatch, t) => {
+  const { id, name, removable } = channel;
   const activeClassName = cn('btn', {
-    'btn-secondary': isActive,
-    'btn-outline-secondary': !isActive,
+    'btn-secondary': id === currentChannelId,
+    'btn-outline-secondary': id !== currentChannelId,
   });
 
-  return (
-    <button
-      onClick={() => onClick(id)}
-      type="button"
-      className={`w-100 text-start rounded-0 ${activeClassName} text-truncate overflow-hidden`}
-    >
-      <span className="me-1 text-truncate overflow-hidden">
-        {t('channels.id')}
-      </span>
-      {name}
-    </button>
-  );
-};
-
-const RemovableChannel = ({
-  id, name, currentChannelId, t, dispatch,
-}) => {
-  const handlerVariant = () => (id === currentChannelId ? 'secondary' : 'outline-secondary');
+  const hendlerVariant = () => (id === currentChannelId ? 'secondary' : 'outline-secondary');
 
   const handleChannelClick = () => {
     dispatch(setCurrentChannelId(id));
   };
 
-  const handleRenameClick = () => {
-    dispatch(showModal({ modalType: 'renaming', channelId: id }));
-  };
-
-  const handleDeleteClick = () => {
-    dispatch(showModal({ modalType: 'removing', channelId: id }));
-  };
+  if (!removable) {
+    return (
+      <li key={id} className="nav-item w-100">
+        <button
+          onClick={handleChannelClick}
+          type="button"
+          className={`w-100 text-start rounded-0 ${activeClassName} text-truncate overflow-hidden`}
+        >
+          <span className="me-1 text-truncate overflow-hidden">
+            {t('channels.id')}
+          </span>
+          {name}
+        </button>
+      </li>
+    );
+  }
 
   return (
     <li key={id} className="nav-item w-100">
-      <div className="d-flex align-items-center">
-        <div className="flex-grow-1">
+      <div role="group" className="d-flex dropdown btn-group">
+        <Dropdown as={ButtonGroup} className="w-100">
           <Button
-            variant={handlerVariant()}
+            variant={hendlerVariant()}
             className="w-100 rounded-0 text-start text-truncate"
             onClick={handleChannelClick}
           >
-            #
-            {' '}
             {name}
           </Button>
-        </div>
-        <Dropdown className="ml-2">
+
           <Dropdown.Toggle
-            variant={handlerVariant()}
-            id={`dropdown-split-${id}`}
+            split
+            variant={hendlerVariant()}
+            id="dropdown-split-basic"
           >
-            <span className="visually-hidden">
-              {t('channels.dropdownLabel')}
-            </span>
+            <span className="visually-hidden">{t('channels.dropdownLabel')}</span>
           </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={handleRenameClick}>
+          <Dropdown.Menu onClick={handleChannelClick}>
+            <Dropdown.Item
+              onClick={() => dispatch(showModal({ modalType: 'renaming', channelId: id }))}
+            >
               {t('channels.rename')}
             </Dropdown.Item>
-            <Dropdown.Item onClick={handleDeleteClick}>
+            <Dropdown.Item
+              onClick={() => dispatch(showModal({ modalType: 'removing', channelId: id }))}
+            >
               {t('channels.delete')}
             </Dropdown.Item>
           </Dropdown.Menu>
@@ -86,15 +75,27 @@ const RemovableChannel = ({
   );
 };
 
-const MainCol = ({
-  handleAddChannelClick, renderChannels, t, currentChannelId,
-}) => (
-  <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-    <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-      <b>{t('channels.title')}</b>
-      {currentChannelId && (
+const renderChannelsList = (channels, currentChannelId, dispatch, t) => channels.map((channel) => (
+  renderChannelItem(channel, currentChannelId, dispatch, t)
+));
+
+const Channels = () => {
+  const dispatch = useDispatch();
+  const { channels, currentChannelId } = useSelector((state) => state.channels);
+  const { t } = useTranslation();
+
+  const handleAddChannel = () => {
+    dispatch(showModal({ modalType: 'adding', channelId: null }));
+  };
+
+  return (
+    <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+      <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+        <b>
+          {t('channels.title')}
+        </b>
         <button
-          onClick={handleAddChannelClick}
+          onClick={handleAddChannel}
           type="button"
           className="p-0 text-primary btn btn-group-vertical"
         >
@@ -108,79 +109,18 @@ const MainCol = ({
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
           </svg>
-          <span className="visually-hidden">{t('channels.plus')}</span>
+          <span className="visually-hidden">
+            {t('channels.plus')}
+          </span>
         </button>
-      )}
-    </div>
-    <ul
-      id="channels-box"
-      className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
-    >
-      {renderChannels()}
-    </ul>
-  </Col>
-);
-
-const renderChannels = (
-  channels,
-  t,
-  currentChannelId,
-  handleChannelClick,
-  dispatch,
-) => channels.map(({ id, name, removable }) => {
-  if (!removable) {
-    return (
-      <li key={id} className="nav-item w-100">
-        <ChannelButton
-          id={id}
-          name={name}
-          currentChannelId={currentChannelId}
-          t={t}
-          onClick={handleChannelClick}
-        />
-      </li>
-    );
-  }
-
-  return (
-    <RemovableChannel
-      key={id}
-      id={id}
-      name={name}
-      currentChannelId={currentChannelId}
-      t={t}
-      dispatch={dispatch}
-    />
-  );
-});
-
-const Channels = () => {
-  const dispatch = useDispatch();
-  const channels = useSelector(selectChannels);
-  const currentChannelId = useSelector(selectCurrentChannelId);
-  const { t } = useTranslation();
-
-  const handleChannelClick = (id) => {
-    dispatch(setCurrentChannelId(id));
-  };
-
-  const handleAddChannelClick = () => {
-    dispatch(showModal({ modalType: 'adding', channelId: null }));
-  };
-
-  return (
-    <MainCol
-      handleAddChannelClick={handleAddChannelClick}
-      renderChannels={() => renderChannels(
-        channels,
-        t,
-        currentChannelId,
-        handleChannelClick,
-        dispatch,
-      )}
-      t={t}
-      currentChannelId={currentChannelId}
-    />
+      </div>
+      <ul
+        id="channels-box"
+        className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
+      >
+        {renderChannelsList(channels, currentChannelId, dispatch, t)}
+      </ul>
+    </Col>
   );
 };
 
