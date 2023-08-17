a4c382cea62e1e7ebfd05eb3939086a849cce2d3
aliceicelie <aliceicelie@gmail.com>
2023-08-17 11:21:24 +0300

    аix routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import AuthProvider from './AuthProvider';
-import PrivateRoute from './PrivateRoute';
-import ChatPage from './ChatPage';
-import LoginPage from './LoginPage';
-import NotFoundPage from './NotFoundPage';
-import SignUpPage from './SignUpPage';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from '../utils/appRoutes';
 
 const App = () => (
   <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<ChatPage />} />
-        </Route>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </AuthProvider>
+    <AppRoutes />
   </BrowserRouter>
 );
 
---
+++ frontend/src/utils/appRoutes.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import AuthProvider from './AuthProvider';
+import PrivateRoute from './PrivateRoute';
+import ChatPage from './ChatPage';
+import LoginPage from './LoginPage';
+import NotFoundPage from './NotFoundPage';
+import SignUpPage from './SignUpPage';
+
+const AppRoutes = () => (
+  <AuthProvider>
+    <Routes>
+      <Route element={<PrivateRoute />}>
+        <Route path="/" element={<ChatPage />} />
+      </Route>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
+  </AuthProvider>
+);
+
+export default AppRoutes;
